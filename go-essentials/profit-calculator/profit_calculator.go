25cee580f2ae308354e7df51a3338add6b2e1bc2
grvlcd <generalpride@gmail.com>
2026-02-26 21:23:40 +0800

    refactor profit calculator

--- go-essentials/profit-calculator/profit_calculator.go
+++ go-essentials/profit-calculator/profit_calculator.go
@@ -7,20 +7,28 @@ import (
 func main() {
 	var revenue, expenses, taxRate float64
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	printAndScan("Enter revenue: ", &revenue)
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	printAndScan("Enter expenses: ", &expenses)
 
-	fmt.Print("Enter taxRate: ")
-	fmt.Scan(&taxRate)
+	printAndScan("Enter taxRate: ", &taxRate)
 
-	var earningsBeforeTax = revenue - expenses
-	var earningsAfterTax = earningsBeforeTax * (1 - taxRate/100)
-	var ratio = earningsBeforeTax / earningsAfterTax
+	earningsBeforeTax, earningsAfterTax, ratio := calculateEarningsAndRatio(revenue, expenses, taxRate)
 
-	fmt.Printf("Earnings Before Tax: %f\n", earningsBeforeTax)
-	fmt.Printf("Earnings After Tax: %f\n", earningsAfterTax)
-	fmt.Printf("Ratio: %f\n", ratio)
+	fmt.Printf("Earnings Before Tax: %.2f\n", earningsBeforeTax)
+	fmt.Printf("Earnings After Tax: %.2f\n", earningsAfterTax)
+	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func printAndScan(label string, value *float64) {
+	fmt.Print(label)
+	fmt.Scan(value)
+}
+
+func calculateEarningsAndRatio(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	eat := ebt * (1 - taxRate/100)
+	ratio := ebt / eat
+
+	return ebt, eat, ratio
 }
