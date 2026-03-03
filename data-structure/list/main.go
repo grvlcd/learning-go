f3f12173c83a30c880ff3a862e4426cd82dc15dc
grvlcd <generalpride@gmail.com>
2026-03-04 02:41:47 +0800

    assigning and getting value from array

--- data-structure/list/main.go
+++ data-structure/list/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99, 9.99, 20.50, 12.99}
-	fmt.Println(prices)
+	productNames[2] = "A Carpet"
+	fmt.Println(prices[2])
+	fmt.Println(productNames)
 }
