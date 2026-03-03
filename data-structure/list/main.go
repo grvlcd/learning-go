4696c03f1d9ef89dae52b4b38e6be9d5249e5157
grvlcd <generalpride@gmail.com>
2026-03-04 02:44:38 +0800

    array slices

--- data-structure/list/main.go
+++ data-structure/list/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	var productNames [4]string = [4]string{"A Book"}
+
 	prices := [4]float64{10.99, 9.99, 20.50, 12.99}
+
 	productNames[2] = "A Carpet"
+
+	featuredPrices := prices[1:3]
+
 	fmt.Println(prices[2])
 	fmt.Println(productNames)
+	fmt.Println(featuredPrices)
 }
